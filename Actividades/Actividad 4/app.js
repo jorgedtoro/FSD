9d2c8f9712ce6ebf44d78f68b56c6c93306552ec
Jorge de Toro <jorgedtoro@gmail.com>
2022-05-11 16:40:24 +0200

    update 11-05
    actividad 4. new functions

--- Actividades/Actividad 4/app.js
+++ Actividades/Actividad 4/app.js
@@ -1,3 +1,60 @@
+// fetch("data.json")
+//   .then((response) => response.json())
+//   .then((data) => console.log(data));
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  fetchData();
+});
+const templateProduct = document.getElementById("first-product");
+
+// function PintarProductos() {
+//   const lista = document.getElementById("lista-dinamica");
+
+//   const arrayItem = ["item 1", "item 2", "item 3"];
+
+//   const fragment = document.createDocumentFragment();
+
+//   arrayItem.forEach((item) => {
+//     const li = document.createElement("li");
+//     li.textContent = item;
+//     fragment.appendChild(li);
+//   });
+
+//   lista.appendChild(fragment);
+// }
+
+// function PintarProductos() {
+//   const lista = document.getElementById("first-product");
+
+//   const arrayItem = ["iFhone 13 Pro", "0K3QOSOV4V", "938.99"];
+
+//   const fragment = document.createDocumentFragment();
+
+//   arrayItem.forEach((item) => {
+//     const h2 = document.createElement("h2");
+//     h2.textContent = item;
+//     fragment.appendChild(h2);
+//   });
+//   lista.appendChild(fragment);
+// }
+
+// Traer productos
+const fetchData = async () => {
+  const res = await fetch("data.json");
+  const data = await res.json();
+  // console.log(data)
+  pintaDatos(data);
+};
+
+const pintaDatos = (data) => {
+  data.forEach((item) => {
+    templateProduct.querySelector("h2").textContent = item.title;
+    const clone = templateCard.cloneNode(true);
+    fragment.appendChild(clone);
+  });
+  cards.appendChild(fragment);
+};
+
 //clase carrito
 
 class Carrito {
@@ -34,3 +91,5 @@ class Carrito {
     // }
   }
 }
+
+// http://jsonblob.com/973862124204081152.
