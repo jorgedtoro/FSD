940323af768253b4b94131c1edcb3891fd0abccb
c0k3s <jorgedtoro@gmail.com>
2022-05-15 12:06:56 +0200

    Actividad4 json y js
    se trae con éxito los datos desde json con js

--- Actividades/Actividad 4/app.js
+++ Actividades/Actividad 4/app.js
@@ -1,95 +1,33 @@
-// fetch("data.json")
-//   .then((response) => response.json())
-//   .then((data) => console.log(data));
-
 document.addEventListener("DOMContentLoaded", (e) => {
   fetchData();
 });
-const templateProduct = document.getElementById("first-product");
+const templateProduct = document.getElementById(
+  "pruebatemplate_product"
+).content;
+const tbody = document.getElementById("body_products");
+const fragment = document.createDocumentFragment();
 
-// function PintarProductos() {
-//   const lista = document.getElementById("lista-dinamica");
-
-//   const arrayItem = ["item 1", "item 2", "item 3"];
-
-//   const fragment = document.createDocumentFragment();
-
-//   arrayItem.forEach((item) => {
-//     const li = document.createElement("li");
-//     li.textContent = item;
-//     fragment.appendChild(li);
-//   });
-
-//   lista.appendChild(fragment);
-// }
-
-// function PintarProductos() {
-//   const lista = document.getElementById("first-product");
-
-//   const arrayItem = ["iFhone 13 Pro", "0K3QOSOV4V", "938.99"];
-
-//   const fragment = document.createDocumentFragment();
-
-//   arrayItem.forEach((item) => {
-//     const h2 = document.createElement("h2");
-//     h2.textContent = item;
-//     fragment.appendChild(h2);
-//   });
-//   lista.appendChild(fragment);
-// }
-
-// Traer productos
+// Traer productos de la base de datos json
 const fetchData = async () => {
   const res = await fetch("data.json");
   const data = await res.json();
-  // console.log(data)
+  // console.log(data);
   pintaDatos(data);
 };
 
+//creamos los productos en el index.html
 const pintaDatos = (data) => {
   data.forEach((item) => {
-    templateProduct.querySelector("h2").textContent = item.title;
-    const clone = templateCard.cloneNode(true);
+    templateProduct.getElementById("product__title").textContent = item.title;
+    templateProduct.getElementById("product__sku").textContent =
+      "Ref: " + item.SKU;
+    templateProduct.getElementById("product__price").textContent = item.price;
+    templateProduct.getElementById("product__total").textContent = "0€";
+
+    const clone = templateProduct.cloneNode(true);
     fragment.appendChild(clone);
   });
-  cards.appendChild(fragment);
+  tbody.appendChild(fragment);
 };
-
-//clase carrito
-
-class Carrito {
-  constructor(productos) {
-    this.productos = productos;
-  }
-
-  actualizarUnidades(sku, unidades) {
-    // Actualiza el número de unidades que se quieren comprar de un producto
-  }
-
-  obtenerInformacionProducto(sku) {
-    // Devuelve los datos de un producto además de las unidades seleccionadas
-    // Por ejemplo
-    // {
-    //   "sku": "0K3QOSOV4V",
-    //   "quantity": 3
-    // }
-  }
-
-  obtenerCarrito() {
-    // Devuelve información de los productos añadidos al carrito
-    // Además del total calculado de todos los productos
-    // Por ejemplo:
-    // {
-    //   "total": "5820",
-    //   "currency: "€",
-    //   "products" : [
-    //     {
-    //       "sku": "0K3QOSOV4V"
-    //       ..
-    //     }
-    //    ]}
-    // }
-  }
-}
 
 // http://jsonblob.com/973862124204081152.
