11a316edc3d22942b87d8dd374b6d3b8c507eb05
Jorge de Toro <jorgedtoro@gmail.com>
2022-06-01 16:34:17 +0200

    update 01-06 office
    carrito js update bluuweb

--- Actividades/Carrito JS/app.js
+++ Actividades/Carrito JS/app.js
@@ -1,3 +1,5 @@
+const cards = document.getElementById("cards");
+const items = document.getElementById("items");
 const templateCard = document.getElementById("template-card").content;
 
 const templateCarrito = document.getElementById("template-carrito").content;
@@ -10,6 +12,9 @@ let carrito = {};
 // El evento DOMContentLoaded es disparado cuando el documento HTML ha sido completamente cargado y parseado
 document.addEventListener("DOMContentLoaded", (e) => {
   fetchData();
+});
+cards.addEventListener("click", (e) => {
+  addCarrito(e);
 });
 
 // Traer productos
@@ -30,4 +35,56 @@ const pintarCards = (data) => {
     fragment.appendChild(clone);
   });
   cards.appendChild(fragment);
+};
+
+//Agregar al carrito
+const addCarrito = (e) => {
+  if (e.target.classList.contains("btn-dark")) {
+    setCarrito(e.target.parentElement);
+  }
+};
+
+const setCarrito = (item) => {
+  const producto = {
+    title: item.querySelector("h5").textContent,
+    precio: item.querySelector("p").textContent,
+    id: item.querySelector("button").dataset.id,
+    cantidad: 1,
+  };
+  console.log(producto);
+  //si ya existe el producto le sumamos 1 a la cantidad y no lo creamos de nuevo.
+  if (carrito.hasOwnProperty(producto.id)) {
+    producto.cantidad = carrito[producto.id].cantidad + 1;
+  }
+  carrito[producto.id] = { ...producto };
+  pintarCarrito();
+};
+
+//Crear carrito
+const pintarCarrito = () => {
+  items.innerHTML = "";
+
+  Object.values(carrito).forEach((producto) => {
+    templateCarrito.querySelector("th").textContent = producto.id;
+    templateCarrito.querySelectorAll("td")[0].textContent = producto.title;
+    templateCarrito.querySelectorAll("td")[1].textContent = producto.cantidad;
+    templateCarrito.querySelector("span").textContent =
+      producto.precio * producto.cantidad;
+
+    //botones
+    templateCarrito.querySelector(".btn-info").dataset.id = producto.id;
+    templateCarrito.querySelector(".btn-danger").dataset.id = producto.id;
+
+    const clone = templateCarrito.cloneNode(true);
+    fragment.appendChild(clone);
+  });
+  items.appendChild(fragment);
+
+  //pintarFooter()
+};
+
+//crear footer del carrito
+
+const pintarFooter = () => {
+  pintarFooter.innerHTML = "";
 };
